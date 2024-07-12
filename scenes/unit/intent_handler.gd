0f8378133351399f7710f60f57b5ff9ac4aa73d0
santonip <sebastian.santoni@mail.utoronto.ca>
2024-07-12 12:51:03 +0300

    intent ui and intenthandler added, but logic to be implemented after refactoring unit/movehandler

---
+++ scenes/unit/intent_handler.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var intent_ui = $IntentUI
+
+
+func _on_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_mouse_exited():
+	pass # Replace with function body.
+
+
+func update_ui() -> void:
+	pass
+	
+
+func draw_target_arrows() -> void:
+	pass
