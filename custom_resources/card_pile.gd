e9d2064fc2f4b4ad8ba3a3aa08c7054eb35848bb
santonip <sebastian.santoni@mail.utoronto.ca>
2024-07-14 11:55:19 +0300

    card_pile_size_changed signal created and configured. CardPile version 1.0 finished!

--- custom_resources/card_pile.gd
+++ custom_resources/card_pile.gd
@@ -1,16 +1,20 @@
 class_name CardPile
 extends Resource
 
+signal card_pile_size_changed
+
 @export var cards: Array[Resource]
 @export var label_right: bool
 
 
 func add_card(card: Card) -> void:
 	cards.append(card)
+	card_pile_size_changed.emit()
 
 
 func delete_card(card: Card) -> void:
 	cards.erase(card)
+	card_pile_size_changed.emit()
 
 
 func size() -> int:
--- scenes/card_pile/card_pile_handler.gd
+++ scenes/card_pile/card_pile_handler.gd
@@ -1,6 +1,7 @@
 class_name CardPileHandler
 extends Control
 
+
 @export var card_pile: CardPile
 @export var card_pile_icon: Texture
 
@@ -9,6 +10,12 @@ extends Control
 
 
 func _ready() -> void:
+	card_pile.card_pile_size_changed.connect(_on_card_pile_size_changed)
+	
 	card_pile_ui.card_pile = card_pile
 	card_pile_ui.card_pile_handler = self
 	card_pile_ui.load_ui()
+
+
+func _on_card_pile_size_changed() -> void:
+	card_pile_ui.update_count()
